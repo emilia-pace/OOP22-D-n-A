bc1a5c491ac5c3e606a308a1ca75fc3ef80ad434
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2023-04-15 19:13:01 +0200

    added first draft of Button,MovablePlatform,Puddle and Door classes, modified Lever class

--- src/main/java/it/unibo/dna/model/object/Lever.java
+++ src/main/java/it/unibo/dna/model/object/Lever.java
@@ -1,9 +1,8 @@
 package it.unibo.dna.model.object;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
-import org.apache.commons.lang3.text.WordUtils;
 
 import it.unibo.dna.common.Position2d;
 
@@ -11,29 +10,32 @@ public class Lever implements Object{
 
     private boolean state = false;
     private Position2d pos; 
+    private MovablePlatform platform;
+    private static int amount = 10;
 
     public Lever(Position2d pos){
         this.pos=pos;
     }
 
     @Override
-    public Position2d getPosition2d() {
+    public Position2d getPosition() {
         return pos;
     }
 
     @Override
     public void enable() {
         state=true;
+        platform.move(amount);
     }
 
     @Override
     public void disable() {
         state=false;
+        platform.move(-amount);
     }
     @Override
-        public boolean isActivated() {
-            // TODO Auto-generated method stub
-           throw new UnsupportedOperationException("Unimplemented method 'isActivated'");
+    public boolean isActivated() {
+        return state;
     }
 }
 

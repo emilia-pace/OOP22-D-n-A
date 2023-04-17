1efffd2005c0ce93d2b75c24484b38a203f7cf68
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2023-04-17 18:39:40 +0200

    Door: added door types, modified openDoor() method

--- src/main/java/it/unibo/dna/model/object/Door.java
+++ src/main/java/it/unibo/dna/model/object/Door.java
@@ -1,13 +1,19 @@
 package it.unibo.dna.model.object;
 
 import it.unibo.dna.common.Position2d;
+import it.unibo.dna.model.object.Character;
 
 public class Door implements Entity{
+    public static enum doorType {ANGEL_DOOR, DEVIL_DOOR};
 
+    private doorType type;
     private Position2d pos;
+    private boolean isOpen=false;
+    
 
-    public Door(Position2d pos){
-        
+    public Door(Position2d pos,doorType type) {
+        this.pos=pos;
+        this.type=type;
     }
 
     @Override
@@ -15,8 +21,15 @@ public class Door implements Entity{
         return pos;
     }
 
-    public void openDoor(){
-        
+    public void openDoor(Character c) {
+        /*switch(c.getType){
+            case ANGEL: if(this.type.equals(ANGEL_DOOR)){
+                isOpen=true;
+            }
+            case DEVIL: if(this.type.equals(DEVIL_DOOR)){
+                isOpen=true;
+            }
+        }*/
     }
     
 }

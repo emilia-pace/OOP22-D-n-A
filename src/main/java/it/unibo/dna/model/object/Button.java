f1b51971ebc1f8ba275d9db6883e12edeeeb965d
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2023-04-18 17:40:31 +0200

    Button & MovablePlatform: -cambiato nome di 'on' in 'isPressed' -aggiunto costruttore -aggiunti getter e setter per Position e MovablePlatform -aggiunti i metodi enable(), disable(), isActivated(), push(), returnToOriginalPosition() -in MovablePlatform, aggiunti finalPos, setter per position, setter e getter per finalPos.

--- src/main/java/it/unibo/dna/model/object/Button.java
+++ src/main/java/it/unibo/dna/model/object/Button.java
@@ -5,30 +5,66 @@ import it.unibo.dna.common.Position2d;
 public class Button implements GameObject{
 
     private Position2d pos;
-    private boolean on = false;
-    private MovablePlatform mp;
+    private boolean isActive = false;
+    private boolean isPressed = false;
+    private boolean isPushed = false;
     private static Integer amount = 5;
+
+    private MovablePlatform mp;
+
+    public Button(Position2d pos, MovablePlatform mp) {
+        this.pos=pos;
+        this.mp=mp;
+    }
 
     @Override
     public Position2d getPosition() {
         return pos;
     }
 
-    @Override
-    public void enable() {
-        on=true;
-        mp.move(amount);
+    public void setPosition(Position2d position) {
+        pos=position;
+    }
+
+    public MovablePlatform getMovablePlatform() {
+        return mp;
+    }
+
+    public void setMovablePlatform(MovablePlatform m) {
+        mp=m;
     }
 
     @Override
-    public void disable() {
-        on=false;
-        mp.move(-amount);
+    public void enable() { //abilita il bottone, e tutte le sue funzionalità
+        if(!isActive){
+            isActive=true;
+        }
     }
 
     @Override
-    public boolean isActivated() {
-        return on;
+    public void disable() { // disabilita il bottone, e tutte le sue funzionalità
+        if(isActive){
+            isActive=false;
+        }
+    }
+
+    @Override
+    public boolean isActivated() { // dice se il bottone è abilitato o meno
+        return isActive;
+    }
+
+    public void push(){ //bottone spinto. La piattaforma si muove fintanto che il bottone sta venendo spinto. 
+                        //Quando si scende dal bottone, la piattaforma torna alla sua pos originale:
+        while(isPushed && !mp.getPosition().equals(mp.getFinalPosition())){
+            mp.setPosition(new Position2d(mp.getPosition().x+1, mp.getPosition().x));
+        }
+
+    }
+
+    public void returnToOriginalPosition(){ 
+        while(!isPushed && !mp.getPosition().equals(mp.getOriginalPos())){
+            mp.setPosition(new Position2d(mp.getPosition().x-1, mp.getPosition().y));
+        }
     }
     
 }
--- src/main/java/it/unibo/dna/model/object/MovablePlatform.java
+++ src/main/java/it/unibo/dna/model/object/MovablePlatform.java
@@ -5,10 +5,32 @@ import it.unibo.dna.common.Position2d;
 public class MovablePlatform implements Entity{
 
     private Position2d pos;
+    private Position2d originalPos;
+    private Position2d finalPos;
 
     @Override
     public Position2d getPosition() {
         return pos;
+    }
+
+    public void setPosition(Position2d p) {
+        pos=p;
+    }
+
+    public Position2d getFinalPosition() {
+        return finalPos;
+    }
+
+    public void setFinalPosition(Position2d fp) {
+        finalPos=fp;
+    }
+
+    public Position2d getOriginalPos() {
+        return originalPos;
+    }
+
+    public void setOriginalPos(Position2d op){
+        originalPos=op;
     }
 
     public void move(double amount){

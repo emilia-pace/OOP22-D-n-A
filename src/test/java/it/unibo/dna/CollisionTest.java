f04199ec267c7dd8c513e74643af0fc4f59e3b31
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2023-05-22 15:51:24 +0200

    CollisionTest: implementati test su collisione bordi e collisione sui lati destro e sinistro

--- src/test/java/it/unibo/dna/CollisionTest.java
+++ src/test/java/it/unibo/dna/CollisionTest.java
@@ -1,25 +1,29 @@
 package it.unibo.dna;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.*;
 
 import it.unibo.dna.common.Position2d;
-//import it.unibo.dna.common.Vector2d;
+import it.unibo.dna.common.Vector2d;
 import it.unibo.dna.model.RectBoundingBox;
-//import it.unibo.dna.model.object.PlayerImpl;
-//import it.unibo.dna.model.object.PlayerImpl.Type;
 import it.unibo.dna.model.object.api.BoundingBox;
-//import it.unibo.dna.model.object.api.Player;
+import it.unibo.dna.model.object.api.Player;
+import it.unibo.dna.model.object.api.Player.Type;
+import it.unibo.dna.model.object.PlayerImpl;
+
 
 public class CollisionTest {
-
-    private static final Position2d POS = new Position2d(0, 0);
+    private static final double X = 10;
+    private static final double Y = 20;
+    private static final Position2d POS = new Position2d(X, Y);
     private static final double HEIGHT = 4;
     private static final double WIDTH = 4;
     private static final BoundingBox BOX = new RectBoundingBox(POS, HEIGHT, WIDTH);
-    /*private static final double DIM = 100;
-    private static final Game GAME;
-    private static final Player CHARACTER = new PlayerImpl(POS, new Vector2d(0, 0), HEIGHT, WIDTH, Type.ANGEL);*/
+    private static final int GAMEHEIGHT = 400;
+    private static final int GAMEWIDTH = 400;
+    private static final Game GAME = new Game(GAMEWIDTH, GAMEHEIGHT, 0);
+    private static final Player CHARACTER = new PlayerImpl(POS, new Vector2d(0, 0), HEIGHT, WIDTH, Type.ANGEL);
 
     /**
      * test the collision between rectangular boxes
@@ -29,19 +33,43 @@ public class CollisionTest {
         //(0,0) angolo in alto a sx
         assertTrue(BOX.isCollidingWith(POS, HEIGHT, WIDTH));
         assertTrue(BOX.isCollidingWith(POS, HEIGHT / 2, WIDTH / 2));
-        assertTrue(BOX.isCollidingWith(new Position2d(WIDTH, HEIGHT), HEIGHT, WIDTH));
-        assertTrue(BOX.isCollidingWith(new Position2d(WIDTH, 0), HEIGHT, WIDTH));
-        assertTrue(BOX.isCollidingWith(new Position2d(0, HEIGHT), HEIGHT, WIDTH));
+        assertTrue(BOX.isCollidingWith(new Position2d(X - WIDTH, Y), HEIGHT, WIDTH));
+        assertTrue(BOX.isCollidingWith(new Position2d(X, Y - HEIGHT), HEIGHT, WIDTH));
+        assertTrue(BOX.isCollidingWith(new Position2d(X + WIDTH, Y), HEIGHT, WIDTH));
+        assertTrue(BOX.isCollidingWith(new Position2d(X, Y + HEIGHT), HEIGHT, WIDTH));
 
-        assertFalse(BOX.isCollidingWith(new Position2d(WIDTH + 1, HEIGHT + 1), HEIGHT, WIDTH));
-        assertFalse(BOX.isCollidingWith(new Position2d(WIDTH + 1, 0), HEIGHT / 2, WIDTH / 2));
-        
+        assertFalse(BOX.isCollidingWith(new Position2d(X, 0), HEIGHT, WIDTH));
+        assertFalse(BOX.isCollidingWith(new Position2d(0, Y), HEIGHT, WIDTH));
+        assertFalse(BOX.isCollidingWith(new Position2d(X + WIDTH + 1, Y), HEIGHT, WIDTH));
+        assertFalse(BOX.isCollidingWith(new Position2d(X, Y + HEIGHT + 1), HEIGHT, WIDTH));
+        assertFalse(BOX.isCollidingWith(new Position2d(X - WIDTH - 1, Y), HEIGHT, WIDTH));
+        assertFalse(BOX.isCollidingWith(new Position2d(X, Y - HEIGHT - 1), HEIGHT, WIDTH));
+    }
+
+    /**
+     * test the collision between rectangular boxes
+     */
+    @Test
+    public void testSideCollision() {
+        //(0,0) angolo in alto a sx
+        assertTrue(BOX.sideCollision(new Position2d(X + WIDTH, Y), HEIGHT, WIDTH));
+        assertTrue(BOX.sideCollision(new Position2d(X - WIDTH, Y), HEIGHT, WIDTH));
+        assertTrue(BOX.sideCollision(new Position2d(X + WIDTH, Y - 1), HEIGHT, WIDTH));
+        assertTrue(BOX.sideCollision(new Position2d(X - WIDTH, Y + HEIGHT), HEIGHT, WIDTH));
+
+        assertFalse(BOX.sideCollision(POS, HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X + WIDTH + 1, Y), HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X - WIDTH - 1, Y), HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X, Y - HEIGHT), HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X, Y + HEIGHT), HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X + WIDTH + 1, Y + HEIGHT), HEIGHT, WIDTH));
+        assertFalse(BOX.sideCollision(new Position2d(X + WIDTH - 1, Y), HEIGHT / 2, WIDTH / 2));
 
     }
 
     /**
-     * test the collision between the character and the borders
-     
+    * test the collision between the character and the borders
+    */
     @Test
     public void testBordersCollision() {
         //(0,0) angolo in alto a sx
@@ -51,23 +79,23 @@ public class CollisionTest {
         final double southBorderY = GAME.getBoundingBox().getHeight();
 
         CHARACTER.setPosition(new Position2d(eastBorderX - WIDTH - 1, 1));
-        assertFalse(GAME.checkBorders(CHARACTER));
+        assertFalse(GAME.checkVerticalBorders(CHARACTER.getPosition().x, CHARACTER.getBoundingBox().getWidth()));
         CHARACTER.setPosition(new Position2d(eastBorderX - WIDTH, 1));
-        assertTrue(GAME.checkBorders(CHARACTER));
+        assertTrue(GAME.checkVerticalBorders(CHARACTER.getPosition().x, CHARACTER.getBoundingBox().getWidth()));
 
         CHARACTER.setPosition(new Position2d(westBorderX + 1, 1));
-        assertFalse(GAME.checkBorders(CHARACTER));
+        assertFalse(GAME.checkVerticalBorders(CHARACTER.getPosition().x, CHARACTER.getBoundingBox().getWidth()));
         CHARACTER.setPosition(new Position2d(westBorderX, 1));
-        assertTrue(GAME.checkBorders(CHARACTER));
+        assertTrue(GAME.checkVerticalBorders(CHARACTER.getPosition().x, CHARACTER.getBoundingBox().getWidth()));
 
         CHARACTER.setPosition(new Position2d(1, northBorderY + 1));
-        assertFalse(GAME.checkBorders(CHARACTER));
+        assertFalse(GAME.checkHorizontalBorders(CHARACTER.getPosition().y, CHARACTER.getBoundingBox().getHeight()));
         CHARACTER.setPosition(new Position2d(1, northBorderY));
-        assertTrue(GAME.checkBorders(CHARACTER));
+        assertTrue(GAME.checkHorizontalBorders(CHARACTER.getPosition().y, CHARACTER.getBoundingBox().getHeight()));
 
-        CHARACTER.setPosition(new Position2d(1, southBorderY + HEIGHT - 1));
-        assertFalse(GAME.checkBorders(CHARACTER));
+        CHARACTER.setPosition(new Position2d(1, southBorderY - HEIGHT - 1));
+        assertFalse(GAME.checkHorizontalBorders(CHARACTER.getPosition().y, CHARACTER.getBoundingBox().getHeight()));
         CHARACTER.setPosition(new Position2d(1, southBorderY - HEIGHT));
-        assertTrue(GAME.checkBorders(CHARACTER));
-    }*/
+        assertTrue(GAME.checkHorizontalBorders(CHARACTER.getPosition().y, CHARACTER.getBoundingBox().getHeight()));
+    }
 }

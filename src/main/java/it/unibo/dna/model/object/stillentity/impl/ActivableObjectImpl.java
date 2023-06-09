75b430b1be0023acba4c37d9d94d90424d6f72ba
GaiaMazzoni <gaia.mazzoni2@studio.unibo.it>
2023-06-09 16:17:47 +0200

    checkstyle

--- src/main/java/it/unibo/dna/model/object/stillentity/impl/ActivableObjectImpl.java
+++ src/main/java/it/unibo/dna/model/object/stillentity/impl/ActivableObjectImpl.java
@@ -27,7 +27,8 @@ public class ActivableObjectImpl extends  AbstractEntity implements ActivableObj
      * @param movablePlatform the {@link MovablePlatform} that the ActivableObject moves
      * @param type the type of the EntityType of the ActivableObject (it can be BUTTON or LEVER)
      */
-    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "cannot pass a copy of the MovablePlatform, it needs to be modified")
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "cannot pass a copy of the MovablePlatform," 
+        + "it needs to be modified")
     public ActivableObjectImpl(final Position2d pos, final Double height, final Double width, 
                                 final MovablePlatform movablePlatform, final EntityType type) {
        super(pos, height, width, type);
@@ -58,7 +59,8 @@ public class ActivableObjectImpl extends  AbstractEntity implements ActivableObj
     /** 
      * @return the {@link MovablePlatform} controlled by the ActivableObject
      */
-    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "cannot pass a copy of the MovablePlatform, it needs to be modified")
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "cannot pass a copy of the MovablePlatform, "
+        + "it needs to be modified")
     public MovablePlatform getMovablePlatform() {
         return this.movablePlatform;
     }
